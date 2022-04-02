fda5c35604250c111fd180e31dbbc6bba538ce03
Andy Bell <andyjsbell@gmail.com>
2022-04-02 15:36:12 +0200

    create res folder

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@ TARGET="${CARGO_TARGET_DIR:-target}"
 set -e
 cd "`dirname $0`"
 cargo build --target wasm32-unknown-unknown --release
+mkdir res
 cp $TARGET/wasm32-unknown-unknown/release/near_game.wasm ./res/
 #wasm-opt -Oz --output ./res/near_game.wasm ./res/near_game.wasm
 
